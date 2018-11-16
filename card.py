de79c7974f654442cd6dc54a1f0278e663a617a0
Jonathan <jonathan@iMac9.local>
2018-11-16 13:49:58 -0500

    Writing a new method to compare cards

--- card.py
+++ card.py
@@ -4,3 +4,14 @@ class Card:
 
         self.rank = rank
         self.suit = suit
+
+    def compared_to(self, other_card):
+        if self.rank > other_card.rank:
+            return self
+        elif other_card.rank > self.rank:
+            return other_card
+        else:
+            if self.suit > other_card():
+                return self
+            else:
+                return other_card
