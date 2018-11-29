faa6474b251d7d905ba1527b10d19f173a8a9a50
Jonathan <jonathan@iMac9.local>
2018-11-29 10:13:45 -0500

    Finish the function by using loops and the compare function in the card method

--- compare.py
+++ compare.py
@@ -1,4 +1,3 @@
-import card
 import deck
 
 
@@ -10,10 +9,31 @@ def main():
     for x in range(5):
         player_one.append(deck_of_cards.draw_a_card())
         player_two.append(deck_of_cards.draw_a_card())
+    player_one_record = 0
+    player_two_record = 0
     games = -1
-    for x in range(5):
+    for rounds in range(5):
         games = games + 1
-        print(player_one[games].compared_to(player_two[games]))
+        print("Player One has " + str(player_one[games]) + ".")
+        print("Player Two has " + str(player_two[games]) + ".")
+        if player_one[games].rank > player_two[games].rank:
+            print("Player One wins the round with " + str(player_one[games].compared_to(player_two[games])) + ".")
+            player_one_record = player_one_record + 1
+        elif player_two[games].rank > player_one[games].rank:
+            print("Player Two wins the round with " + str(player_one[games].compared_to(player_two[games])) + ".")
+            player_two_record = player_two_record + 1
+        else:
+            if player_one[games].suit > player_two[games].suit:
+                print("Player One wins the round with " + str(player_one[games].compared_to(player_two[games])) + ".")
+                player_one_record = player_one_record + 1
+            else:
+                print("Player Two wins the round with " + str(player_one[games].compared_to(player_two[games])) + ".")
+                player_two_record = player_two_record + 1
+        print("")
+    if player_one_record > player_two_record:
+        print("Player One wins the game by", player_one_record, "to " + str(player_two_record) + ".")
+    else:
+        print("Player Two wins the game by", player_two_record, "to " + str(player_one_record) + ".")
 
 
 main()
